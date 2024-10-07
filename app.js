ad9ac2e1e2648b8c5f08f61c447354611a4e2443
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-10-07 22:41:11 +0100

    Make ball move and reflect off of canvas boundaries

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
+    let raf;
 
     let ball = {
         x: 100,
         y: 450,
+        vx: 5,
+        vy: 2,
         radius: 30,
-        color: 'blue'
+        color: 'blue',
+
+        draw() {
+            ctx.beginPath();
+            ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI *2, true);
+            ctx.closePath();
+            ctx.fillStyle = ball.color;
+            ctx.fill();
+        },
     };
 
-    function drawBall() {
+    function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI *2);
-        ctx.fillStyle = ball.color;
-        ctx.fill();
-        ctx.closePath;
+        ball.draw();
+        ball.x += ball.vx;
+        ball.y += ball.vy;
+
+        if (ball.y + ball.vy > canvas.height - ball.radius || ball.y + ball.vy < ball.radius) {
+            ball.vy = -ball.vy;
+        } 
+        if (ball.x + ball.vx > canvas.width - ball.radius || ball.x + ball.vx < ball.radius) {
+            ball.vx = -ball.vx;
+        }
+
+        raf = window.requestAnimationFrame(draw);
     }
 
-    drawBall();
+    canvas.addEventListener('mouseover', (e) => {
+        raf = window.requestAnimationFrame(draw);
+    });
+
+    canvas.addEventListener('mouseout', (e) => {
+        window.cancelAnimationFrame(raf);
+    });
+
+    ball.draw();
 })
